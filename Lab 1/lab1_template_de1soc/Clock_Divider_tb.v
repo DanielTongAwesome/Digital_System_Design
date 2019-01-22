e455d1696605a038df775664e4ebe059b9c10faa
DanielTongAwesome <danieltongubc@gmail.com>
2019-01-21 18:15:17 -0800

    [#5] Create the clock_divider testbench code

---
+++ Lab 1/lab1_template_de1soc/Clock_Divider_tb.v
@@ -0,0 +1,38 @@
+
+/*
+Clock Divider Test Bench Code
+*/
+module Clock_Divider_Test();
+    reg clock_in;
+    reg reset;
+    reg [31:0] count_end;
+    
+    wire clock_out;
+
+    Clock_Divider DUT( .clock_in(clock_in),
+                       .clock_out(clock_out),
+                       .reset(reset),
+                       .count_end(count_end));
+                    
+    initial begin 
+        clock_in = 0;
+        
+        forever begin
+            clock_in = 0;
+            #10;
+            clock_in = 1;
+            #10;
+        end
+    end
+
+    initial begin
+        reset = 1'b1;
+        count_end = 32'd2;
+        #100;
+        count_end = 32'd3;
+        #100;
+        count_end = 32'd4;
+        #100;
+        $stop;
+    end
+endmodule
