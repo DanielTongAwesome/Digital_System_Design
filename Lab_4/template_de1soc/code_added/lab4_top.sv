5559f54c00e3133e49949f83a3d7f89fd16d0800
DanielTongAwesome <danieltongubc@gmail.com>
2019-03-09 16:12:56 -0800

    [#17] test code added

---
+++ Lab_4/template_de1soc/code_added/lab4_top.sv
@@ -0,0 +1,46 @@
+/*
+ * Filename: /Users/DanielTong/Library/Mobile Documents/com~apple~CloudDocs/Study/CPEN311/CPEN311_Lab_Assignment/Lab_4/template_de1soc/code_added/lab4_top.v
+ * Path: /Users/DanielTong/Library/Mobile Documents/com~apple~CloudDocs/Study/CPEN311/CPEN311_Lab_Assignment/Lab_4/template_de1soc/code_added
+ * Created Date: Saturday, March 9th 2019, 2:52:38 pm
+ * Author: DanielTong
+ * 
+ * Purpose: The top-level entity of the project
+ */
+                    // input
+ module lab4_top(   input 				CLOCK_50,
+				    input	[3:0]		KEY,
+				    input	[9:0]		SW,
+				    // output
+                    output	wire [9:0]	LEDR,
+				    output	wire [6:0]	HEX0,
+				    output	wire [6:0]	HEX1,
+				    output	wire [6:0]	HEX2,
+				    output	wire [6:0]	HEX3,
+				    output	wire [6:0]	HEX4,
+				    output	wire [6:0]	HEX5))
+
+    logic clk = CLOCK_50;
+	logic reset = ~KEY[3];
+	logic start = ~KEY[0];
+
+	//memory relevant logic wire
+	logic [7:0] s_memory_address, s_memory_data, s_memory_q;
+	logic s_memory_written_enable;
+	logic s_memory_init_finish;
+	s_memory
+	s_memory_inst(	.address(s_memory_address),
+					.clock	(clk),
+					.data	(s_memory_data),
+					.wren	(s_memory_written_enable),
+					.q		(s_memory_q));
+	
+	s_memory_init
+	s_memory_init_inst(	.clk			(clk),
+						.reset			(1'b0),
+						.start			(start),
+						.written_address(s_memory_address),
+						.data			(s_memory_data),
+						.written_enable	(s_memory_written_enable),
+						.finish			(s_memory_init_finish));
+
+ endmodule
---
+++ Lab_4/template_de1soc/code_added/s_memory_init.sv
@@ -0,0 +1,66 @@
+/*
+ * Filename: /Users/DanielTong/Library/Mobile Documents/com~apple~CloudDocs/Study/CPEN311/CPEN311_Lab_Assignment/Lab_4/template_de1soc/code_added/s_memory_init.sv
+ * Path: /Users/DanielTong/Library/Mobile Documents/com~apple~CloudDocs/Study/CPEN311/CPEN311_Lab_Assignment/Lab_4/template_de1soc/code_added
+ * Created Date: Saturday, March 9th 2019, 3:11:43 pm
+ * Author: DanielTong
+ * 
+ * Purpose: task1 of cpen311 lab4, need to initialize the memory, start from address 0 -> 255
+ *          each address assign value from 0 -> 255
+ */
+                        // input
+module s_memory_init(   input clk,
+                        input reset,
+                        input start,
+                        // output
+                        output [7:0] written_address,
+                        output [7:0] data,
+                        output written_enable,
+                        output finish);
+    
+    // counter - count address and output data
+    logic counter 
+
+    // state define
+    typedef enum logic [1:0] {  
+        IDLE    = 2'b0_0,
+        START   = 2'b1_0,
+        FINISH  = 2'b1_1
+    } stateType;    
+    stateType state = IDLE;
+
+    // output logic
+    assign data = counter;
+    assign written_address = counter;
+    assign written_enable = state[1];
+    assign finish = state[0];
+
+    // state transaction
+    always @(posedge clk) begin
+        if (reset):
+            state <= IDLE;
+        else
+            case(state)
+                // STATE - IDLE 
+                IDLE:   if (start): state <= START;
+                        else state <= IDLE;
+                
+                // STATE - START
+                START:  if (counter == 8'd255) begin
+                            state <= FINISH;
+                            counter <= 8'b0;
+                        end
+                        else begin
+                            counter = counter + 1'b1; 
+                        end
+                
+                // STATE - FINISH
+                FINISH: state <= IDLE;
+                
+                default: state <= IDLE;
+            endcase
+    end
+
+endmodule
+
+    
+    
