e8b6af8e69086e80d8b14549c730eff2c981762a
DanielTongAwesome <danieltongubc@gmail.com>
2019-03-14 22:57:09 -0700

    upload shuffle testbench file

---
+++ Lab_4/template_de1soc/code_added/s_memory_shuffle_tb.sv
@@ -0,0 +1,45 @@
+module s_memory_shuffle_tb();
+
+logic clk, start, reset;
+logic [23:0] secret_key;
+logic [7:0] q;
+
+logic [7:0] address;
+logic [7:0] data;
+logic write_enable, finish;
+
+s_memory_shuffle    DUT(    clk,
+                            start,
+                            reset,
+                            secret_key,
+                            q,
+                            // output
+                            address,
+                            data,
+                            write_enable,
+                            finish);
+
+// clock
+initial begin
+		clk = 0; #10;
+		forever begin
+			clk = 1; #10;
+			clk = 0; #10;
+		end
+end
+
+// logic
+initial begin
+		reset = 1;
+		start = 1;
+		q = 8'd0;
+		secret_key = 24'h249;
+		#10;
+        start = 0;
+		reset = 0;
+		#100;
+		#200;
+		$stop;
+end
+
+endmodule
